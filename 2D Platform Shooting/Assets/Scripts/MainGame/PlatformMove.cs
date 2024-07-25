686e0f7cc99cc7df466e7c230a14208d4d963ac4
DESKTOP-B0964SN\kjy87 <kjy87437902@gmail.com>
2024-07-26 05:32:15 +0900

    07-25-회사 이동플랫폼 순방향만 구현, 구간반복 미구현

--- 2D Platform Shooting/Assets/Scripts/MainGame/PlatformMove.cs
+++ 2D Platform Shooting/Assets/Scripts/MainGame/PlatformMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,28 +6,62 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour
 {
-    [SerializeField] float verticalVelocityMove;//
+    [Header("ÇÃ·§Æû ¼Óµµ")]
+    [SerializeField] float platformSpeed;
+
+    //[Header("ÇÃ·§Æû ¹æÇâ uncheck:¼¼·Î check:°¡·Î")]
+    //[SerializeField] bool changeDir;
+
+    [Header("ÇÃ·§Æû Æ¯Á¤À§Ä¡±îÁö")]
+    [SerializeField] Transform trsLocationStart;
+    [SerializeField] Transform trsLocationEnd;
+    Transform trsRepetition;
+
     Rigidbody2D rigid;
     BoxCollider2D box2coll;
-    Vector2 moveDir;
-    
+    Vector3 platformDir;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        PlayerControll player = collision.GetComponent<PlayerControll>();
-        if (collision.tag == "Player")
-        {
-            moveDir.y = verticalVelocityMove;
-        }
-        
-    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    PlayerControll player = collision.GetComponent<PlayerControll>();
+    //    if (collision.tag == "Player")
+    //    {
+    //        playerCheck = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    PlayerControll player = collision.GetComponent<PlayerControll>();
+    //    if(collision.tag == "Player")
+    //    {
+    //        playerCheck = false;
+    //    }
+    //}
     void Start()
     {
-        
+        transform.position = trsLocationStart.position;
+        platformDir = transform.position;
+        trsRepetition = trsLocationEnd;
     }
 
     void Update()
     {
-        
+        platformMove();
+        //platformRepetition();
+    }
+
+    private void platformMove()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, trsLocationEnd.position, platformSpeed * Time.deltaTime);
+    }
+
+    private void platformRepetition()
+    {
+        if(trsRepetition == trsLocationEnd)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, trsLocationStart.position, platformSpeed * Time.deltaTime);
+        }
     }
 }
